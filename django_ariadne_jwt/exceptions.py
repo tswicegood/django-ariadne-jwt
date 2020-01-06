29efb3f70fdf50b2c6e99656aa4805c59a8597c3
Travis Swicegood <travis@domain51.com>
2020-01-06 10:40:41 -0600

    Update to use gettext_lazy to avoid deprecation warning in Django 3

--- django_ariadne_jwt/exceptions.py
+++ django_ariadne_jwt/exceptions.py
@@ -3,7 +3,7 @@
 from django.core.exceptions import (
     ImproperlyConfigured as DjangoImproperlyConfigured,
 )
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class JSONWebTokenError(Exception):
